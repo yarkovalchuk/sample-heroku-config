634cdbbfa3fac8815a982365120a2ed5cb774ced
y.kovalchuk <fyku>
2018-02-07 17:05:16 +0300

    ertgwrtg

--- server.py
+++ server.py
@@ -5,3 +5,15 @@ while True:
     sleep(10)
     print("Tack!")
     sleep(10)
+
+import requests
+from handler import handle_message
+
+while True:
+    res =requests.get("https://api.telegram.org/bot344998733:AAF0R901Ilq43PH-MSYAnoDHMYrh5yaK3eg/getUpdates")
+    d = res.json()
+    for elem in d["result"]:
+        text = elem["message"]
+        ans =  handle_message(text)
+        chat_id = elem["message"]["chat"]["id"]
+        requests.post("URL", params={"chat_id": chat_id, "text": text} )
