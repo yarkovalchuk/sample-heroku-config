4c22bc5297936e30699ae0688ceb68389e3dd8a9
yarkovalchuk <31861923+yarkovalchuk@users.noreply.github.com>
2018-02-14 17:04:52 +0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,24 +1,14 @@
+from emotions import Emotion
 
 URL = "https://api.telegram.org/bot344998733:AAF0R901Ilq43PH-MSYAnoDHMYrh5yaK3eg/getUpdates"
 
-Emotion = {'angry': "( ` ω ´ )", 'cool': "(◕‿◕)", 'positive': "٩(◕‿◕｡)۶", 'happy': "(〃＾▽＾〃)"}
+# Emotion = {'angry': "( ` ω ´ )", 'cool': "(◕‿◕)", 'positive': "٩(◕‿◕｡)۶", 'happy': "(〃＾▽＾〃)"}
 
 
 def handle_message(message, nickname="user"):
     key = ''
     key = message
     return Emotion.get(key, 'Смайлика не существует :(')
-    '''handles message:
-    @message - text of recieved message
-    @nickname - nickname of sender
-
-    @returns - text of response
-    '''
-
-    answer = 'Hi, ' + nickname + '! '
-    answer += 'Your message is ' + str(len(message)) + ' charachters long.'
-
-    return answer
 
 
 if __name__ == "__main__":
