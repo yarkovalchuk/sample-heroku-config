caff1a0b6da3dc3fe3796b35c6a2ab9b3d84144e
yarkovalchuk <31861923+yarkovalchuk@users.noreply.github.com>
2018-02-14 17:37:21 +0300

    Update server.py

--- server.py
+++ server.py
@@ -12,8 +12,8 @@ while True:
     
     for elem in d["result"]:
         text = elem["message"]
-        try:
-            print(elem["message"]["from"]["username"], text)
+        print(text)
         ans = handle_message(text)
+        print(">>>", ans)
         chat_id = elem["message"]["chat"]["id"]
         requests.post("https://api.telegram.org/bot468389348:AAFk47PMcPIsE_2B5eukGpFUC2CWCGWzdKc/sendMessage", params={"chat_id": chat_id, "text": ans} )
