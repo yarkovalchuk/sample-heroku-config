a33b208a2b71e6fc623ec9eb5fcc6182e9cf3665
yarkovalchuk <31861923+yarkovalchuk@users.noreply.github.com>
2018-03-14 17:05:38 +0300

    Update server.py

--- server.py
+++ server.py
@@ -8,8 +8,12 @@ while True:
     print(d)
 
     for elem in d["result"]:
-        text = elem["message"]["text"]
-        ans = handle_message(text)
         lil = elem["update_id"]+1
-        chat_id = elem["message"]["chat"]["id"]
-        requests.post("https://api.telegram.org/bot468389348:AAFk47PMcPIsE_2B5eukGpFUC2CWCGWzdKc/sendMessage", params={"chat_id": chat_id, "text": ans} )
+        try:
+            text = elem["message"]["text"]
+            ans = handle_message(text)
+            chat_id = elem["message"]["chat"]["id"]
+            requests.post("https://api.telegram.org/bot468389348:AAFk47PMcPIsE_2B5eukGpFUC2CWCGWzdKc/sendMessage", params={"chat_id": chat_id, "text": ans} )
+            except Exception as e:
+                print(e)
+       
