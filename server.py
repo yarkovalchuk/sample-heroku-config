c99451b168897e76f01c3430e626186efaa9f5d7
yarkovalchuk <31861923+yarkovalchuk@users.noreply.github.com>
2018-03-14 16:50:42 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,15 @@
-from time import sleep
-
-
 
 import requests
 from handler import handle_message
-lil= 0 
-
+lil= 0
 while True:
-    res = requests.get("https://api.telegram.org/bot468389348:AAFk47PMcPIsE_2B5eukGpFUC2CWCGWzdKc/getUpdates")
+    res = requests.get("https://api.telegram.org/bot468389348:AAFk47PMcPIsE_2B5eukGpFUC2CWCGWzdKc/getUpdates", params= {"offset": lil})
     d = res.json()
     print(d)
-    
+
     for elem in d["result"]:
-        text = elem["message"]
+        text = elem["message"]["text"]
         ans = handle_message(text)
-        lil= elem["update_id"]+1
+        lil = elem["update_id"]+1
         chat_id = elem["message"]["chat"]["id"]
         requests.post("https://api.telegram.org/bot468389348:AAFk47PMcPIsE_2B5eukGpFUC2CWCGWzdKc/sendMessage", params={"chat_id": chat_id, "text": ans} )
